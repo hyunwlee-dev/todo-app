4db1037d352a0b0b90eec88165713f90c8d192ce
hyunwlee-dev <hwlee3928@gmail.com>
2024-03-01 16:27:52 +0900

    add: LocalRepository class

---
+++ app/utils/LocalRepository.ts
@@ -0,0 +1,17 @@
+import { LocalStorage, Todo } from "@/app/definitions";
+
+export default class LocalStorageImpl implements LocalStorage {
+  private key = "todo";
+
+  save(todos: Todo[]) {
+    localStorage.setItem(this.key, JSON.stringify(todos));
+  }
+
+  get() {
+    return JSON.parse(localStorage.getItem(this.key) || '{}');
+  }
+
+  remove() {
+    return localStorage.removeItem(this.key);
+  }
+}
