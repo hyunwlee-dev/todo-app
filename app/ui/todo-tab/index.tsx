15cfc83dcf5293faeb3cce3403dcc1c7a6e5024e
hyunwlee-dev <hwlee3928@gmail.com>
2024-03-03 18:24:37 +0900

    style: checkbox-button hover gradient, caption in todo-tab

--- app/ui/todo-tab/index.tsx
+++ app/ui/todo-tab/index.tsx
@@ -1,8 +1,8 @@
 import { HTMLAttributes } from "react";
 import clsx from "clsx";
 import { Tab } from "@/app/definitions";
+import { useTodos, useTodosDispatch } from "@/app/contexts/todo.context";
 import styles from "./todo-tab.module.css";
-import { useTodosDispatch } from "@/app/contexts/todo.context";
 
 interface IProps extends HTMLAttributes<HTMLDivElement> {
   tabs: string[];
@@ -18,46 +18,49 @@ export default function TodoTab({
   filteredCnt,
   children
 }: IProps) {
+  const todos = useTodos();
   const dispatch = useTodosDispatch();
   const clearCompleted = () => {
     dispatch({
       type: 'clearCompleted'
     });
   }
-
+  if (todos.length < 1)
+    return null;
   return (
-    <section className={styles['todo-board']}>
-      {children}
-      <div className={styles['todo-tab']}>
-        <div className={styles.count}>
-          <span>{
-            filteredCnt < 1 ?
-              `no left` :
-              `${filteredCnt} items left`
-          }</span>
+    <>
+      <section className={styles['todo-board']}>
+        {children}
+        <div className={styles['todo-tab']}>
+          <div className={styles.count}>
+            <span>{`${filteredCnt} items left`}</span>
+          </div>
+          <div className={styles.tabs}>
+            {
+              tabs.map((tab, idx) =>
+                <button
+                  key={`tab-${tab}`}
+                  className={clsx(styles['tab-button'], { [styles.focus]: Tab[idx] === pickedTab })}
+                  onClick={() => onChangeTab(idx)}
+                >
+                  {tab}
+                </button>
+              )
+            }
+          </div>
+          <div className={styles.buttons}>
+            <button
+              onClick={clearCompleted}
+              className={styles['button-clear-completed']}
+            >
+              Clear Completed
+            </button>
+          </div>
         </div>
-        <div className={styles.tabs}>
-          {
-            tabs.map((tab, idx) =>
-              <button
-                key={`tab-${tab}`}
-                className={clsx(styles['tab-button'], { [styles.focus]: Tab[idx] === pickedTab })}
-                onClick={() => onChangeTab(idx)}
-              >
-                {tab}
-              </button>
-            )
-          }
-        </div>
-        <div className={styles.buttons}>
-          <button
-            onClick={clearCompleted}
-            className={styles['button-clear-completed']}
-          >
-            Clear Completed
-          </button>
-        </div>
+      </section>
+      <div className={styles.caption}>
+        Drag and drop to reorder list
       </div>
-    </section >
+    </>
   );
 }
